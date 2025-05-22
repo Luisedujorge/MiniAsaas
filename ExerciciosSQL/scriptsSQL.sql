543c038a2ac23e0fac92f12d11acd0c57ac0073b
Luís Eduardo <113041853+Luisedujorge@users.noreply.github.com>
2025-05-22 10:12:45 -0300

    Corrigir filtros de cobranças

--- ExerciciosSQL/scriptsSQL.sql
+++ ExerciciosSQL/scriptsSQL.sql
@@ -6,7 +6,7 @@ WHERE value > 1000;
 /* Soma dos valores de cobranças recebidas com a data de criação maior que 2020, agrupadas por tipo de cobrança (billing_type) */
 SELECT billing_type, SUM(value) AS total_received_after_2020
 FROM payment
-WHERE date_created > '2021-01-01 00:00:00'
+WHERE date_created >= '2021-01-01 00:00:00' AND status = 'RECEIVED'
 GROUP BY billing_type;
 
 /* Lista de 20 pagadores (customer_account) com valores recebidos maiores que R$ 1.000,00 */
@@ -14,7 +14,7 @@ SELECT DISTINCT c.name
 FROM customer_account c
 JOIN payment p
 ON p.customer_account_id = c.id
-WHERE p.value > 1000 AND p.payment_date IS NOT NULL
+WHERE p.value > 1000 AND p.status = 'RECEIVED'
 LIMIT 20;
 
 /* Primeiro nome dos clientes (customer) com análise geral aprovada (customer_regiter_status), ordenado por data de aprovação */
